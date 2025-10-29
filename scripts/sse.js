8814fa7780c347e5b4ecc41b55da4eb9e031ce09
ferodoto <ferodoto@vk.com>
2025-10-29 06:31:41 -0500

    updated

--- scripts/sse.js
+++
@@ -1,9 +0,0 @@
-const x = document.getElementById("horaServidor");
-if(typeof(EventSource) !== "undefined") {
-  var source = new EventSource("//www.w3schools.com/html/demo_sse.php");
-  source.onmessage = function(event) {
-    x.innerHTML += event.data + "<br>";
-  };
-} else {
-  x.innerHTML = "Sorry, no support for server-sent events.";
-}
